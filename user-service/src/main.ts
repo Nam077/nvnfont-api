c0b0ddd671a8e083e1d7cceb453580997d1e282a
Nam077 <nam077.me@gmail.com>
2024-10-10 00:11:47 +0700

    refactor(user-service): use ConfigService for RabbitMQ configuration

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Lấy ConfigService từ AppModule
+  const configService = app.get(ConfigService);
+
+  // Sử dụng ConfigService để lấy các giá trị cấu hình
+  const rabbitUser = configService.get<string>('RABBITMQ_USER');
+  const rabbitPass = configService.get<string>('RABBITMQ_PASS');
+  const rabbitHost = configService.get<string>('RABBITMQ_HOST');
+  const rabbitQueue = configService.get<string>('RABBITMQ_QUEUE_USER');
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${rabbitUser}:${rabbitPass}@${rabbitHost}`],
+      queue: rabbitQueue,
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000); // Port của user-service
 }
+
 bootstrap();
