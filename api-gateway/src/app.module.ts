c9d9fd4bbec75e0832e3fadecd44546667a2b0e0
Nam077 <nam077.me@gmail.com>
2024-10-10 00:04:20 +0700

    feat(api-gateway): initial setup for api gateway with RabbitMQ and Redis

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,78 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // ConfigModule có thể sử dụng ở mọi nơi trong ứng dụng
+    }),
+    ClientsModule.registerAsync([
+      {
+        name: 'CATEGORY_TAG_SERVICE', // Tên inject, có thể cố định
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [
+              `amqp://${configService.get('RABBITMQ_USER')}:${configService.get('RABBITMQ_PASS')}@${configService.get('RABBITMQ_HOST')}`,
+            ],
+            queue: configService.get('RABBITMQ_QUEUE_CATEGORY_TAG'), // Tên hàng đợi từ env
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+      {
+        name: 'COMMENT_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [
+              `amqp://${configService.get('RABBITMQ_USER')}:${configService.get('RABBITMQ_PASS')}@${configService.get('RABBITMQ_HOST')}`,
+            ],
+            queue: configService.get('RABBITMQ_QUEUE_COMMENT'), // Tên hàng đợi từ env
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+      {
+        name: 'POST_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [
+              `amqp://${configService.get('RABBITMQ_USER')}:${configService.get('RABBITMQ_PASS')}@${configService.get('RABBITMQ_HOST')}`,
+            ],
+            queue: configService.get('RABBITMQ_QUEUE_POST'), // Tên hàng đợi từ env
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+      {
+        name: 'USER_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [
+              `amqp://${configService.get('RABBITMQ_USER')}:${configService.get('RABBITMQ_PASS')}@${configService.get('RABBITMQ_HOST')}`,
+            ],
+            queue: configService.get('RABBITMQ_QUEUE_USER'), // Tên hàng đợi từ env
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
 })
 export class AppModule {}
